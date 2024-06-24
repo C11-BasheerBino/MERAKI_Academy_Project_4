0e53b21f438d79c9edc2b7e73e869700f472a3de
BasheerBino <basheer.bino@gmail.com>
2024-06-24 20:23:24 +0300

    create design for categories component using MUI

--- frontend/src/components/User interface/Fields.js
+++ frontend/src/components/User interface/Fields.js
@@ -1,33 +1,39 @@
 import axios from "axios";
-import React, { useEffect, useState,createContext } from "react";
-import { Route,Routes, useNavigate } from "react-router-dom";
+import React, { useEffect, useState, createContext } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import ServicesOfField from "./ServicesOfField";
-
-
+import {
+  Stack,
+  Link,
+  ThemeProvider,
+  Container,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material/";
 
 export const ServiceContext = createContext();
 
 const Fields = () => {
- 
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const [allFields, setAllFields] = useState();
-  const [cardId,setCardId]=useState()
-  const [isParentVisible,setIsParentVisible]=useState()
-  const [loggingId,setLoggingId]=useState()
-const goToService=(e)=>{
-setCardId(e.target.id)
-console.log("hello from get");
-navigate("/fields/services")
-}
-  
+  const [cardId, setCardId] = useState();
+  const [isParentVisible, setIsParentVisible] = useState();
+  const [loggingId, setLoggingId] = useState();
+  const goToService = (id) => {
+    setCardId(id);
+    console.log("hello from get");
+    navigate("/fields/services");
+  };
+
   useEffect(() => {
-    setIsParentVisible(true)
-   
+    setIsParentVisible(true);
+
     axios
       .get("http://localhost:5000/fields")
       .then((result) => {
-      
-        console.log("fromfields",result);
+        console.log("fromfields", result);
         setAllFields(result.data.fields);
       })
       .catch((err) => {
@@ -36,24 +42,54 @@ navigate("/fields/services")
   }, []);
 
   return (
-    <div>
-      <ServiceContext.Provider value={{cardId,setCardId,allFields, setAllFields,setIsParentVisible,loggingId}}>
-      <Routes>
-      <Route path="/services" element={<ServicesOfField />} />
-      </Routes>
+   <div>
+      <ServiceContext.Provider
+        value={{
+          cardId,
+          setCardId,
+          allFields,
+          setAllFields,
+          setIsParentVisible,
+          loggingId,
+        }}
+      >
+        <Routes>
+          <Route path="/services" element={<ServicesOfField />} />
+        </Routes>
       </ServiceContext.Provider>
 
-      { isParentVisible && allFields &&
+      <Stack spacing={{ xs: 2, sm: 4 }} direction="row" useFlexGap flexWrap="wrap" justifyContent="center" alignItems="center" mt={4}>
+      {isParentVisible &&
+        allFields &&
         allFields.map((elem) => {
           return (
-            <div>
-              <div id={elem._id} onClick={goToService} style={{ cursor: "pointer" }}>{elem.fieldName}</div>
-              <img src={elem.image} />
-              <div>{elem.description}</div>
-            </div>
+            
+         
+            <Card sx={{ maxWidth: 345 }}>
+              
+              <CardMedia
+                sx={{ height: 140 }}
+                image={elem.image}
+                title={elem.fieldName}
+              />
+
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div" onClick={()=>{
+                  goToService(elem._id)
+                }}
+                style={{ cursor: "pointer" }}>
+                  {elem.fieldName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {elem.description}
+                </Typography>
+              </CardContent>
+            </Card>
+            
           );
         })}
-
+        </Stack>
+        
     </div>
   );
 };
